f8962c1a8a6238e4829d4fe3f41afd9908661631
Esperal <alx.rolland@gmail.com>
2024-05-19 12:05:56 +0200

    Updated project tree

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -4,27 +4,50 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-// OLED constants
-#define SCREEN_WIDTH 128
+////////////
+//
+// CONSTANTS  
+//
+////////////
+
+// OLED 
+#define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 64
-#define OLED_RESET     -1 
+#define OLED_RESET    -1 
+
+// Button
+#define PUSH_BUTTON   25
+
+// Orders
+#define DO_ACTION     "0-DOACTION="
+#define SHOW_DATA     "1-SHOWDATA="
+
+////////////
+//
+// INIT  
+//
+////////////
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-const int push_button = 25; // push button is connected to pin number 25
-int button_state = 0;   // variable to read the status of push button
+// Variable to read the status of push button
+int button_state = 0;   
 
 void setup() {
+
+  // Serial initialization
   Serial.begin(115200);
   Serial.setTimeout(20);
-  pinMode(push_button, INPUT);
+
+  // Push button initialization
+  pinMode(PUSH_BUTTON, INPUT);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
+
   // Initialize screen
   display.clearDisplay();
   display.setTextSize(2);      // 2:1 pixel scale
@@ -34,22 +57,31 @@ void setup() {
   display.display();
 }
 
-void loop() {
-  button_state = digitalRead(push_button);
+////////////
+//
+// LOOP  
+//
+////////////
 
+void loop() {
+
+  // Write on serial interface when button pressed 
+  button_state = digitalRead(PUSH_BUTTON);
   if (button_state == HIGH) {
     delay(500);
-    Serial.println("0-DOACTION");
+    Serial.println(DO_ACTION);
   }
 
+  // When data received through serial interface, show what must be shown on screen
   if (Serial.available() > 0) {
-    String str = Serial.readString();
-    str.trim();
-    Serial.print("Data received : ");
-    Serial.println(str);
+    String serialRead = Serial.readString();
+    serialRead.trim();
 
-    if (str == "1-SHOWDATA") {
-      display.write("Hi daddy");
+    if (serialRead.startsWith(SHOW_DATA)) {
+
+      // Show received data for 3s
+      String rawData = serialRead.substring(11, serialRead.length());
+      display.write(rawData.c_str());
       display.display();
       delay(3000);
       display.setCursor(0, 0);
