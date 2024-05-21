34074dc86d51642212351d7d3cf5d79cc9d3b0b2
Esperal <alx.rolland@gmail.com>
2024-05-22 00:49:20 +0200

    Reordering test files

--- Server/remote_backup_laucher.py
+++ Server/remote_backup_laucher.py
@@ -1,6 +1,8 @@
 # Importing Libraries 
 import serial 
 import time 
+import subprocess
+import logging
 
 serialDevice = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1) 
 
@@ -8,6 +10,9 @@ def sendMessage(messageContent):
     serialDevice.write(bytes("1-SHOWDATA=" + messageContent, 'utf-8')) 
     time.sleep(0.05) 
 
+result = subprocess.run(['bash', './test/fake-script-output.sh'], stdout=subprocess.PIPE)
+
 while True: 
-    messageContent = input("Write something to send to /dev/ttyACM0 : ")
-    sendMessage(messageContent)
+    time.sleep(1);
+    logging.warning(result.stdout.decode('utf-8'))
+    sendMessage(result.stdout.decode('utf-8'))
