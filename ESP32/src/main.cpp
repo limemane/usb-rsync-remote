d5612626c1d7c219bd22dae2294ced2b5d688baf
Esperal <alx.rolland@gmail.com>
2024-05-23 01:11:44 +0200

    ESP32 now able to tell and display when a task is done or when an order is sent. Refactored python script

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -19,8 +19,12 @@
 #define PUSH_BUTTON   25
 
 // Orders
-#define DO_ACTION     "0-DOACTION="
-#define SHOW_DATA     "1-SHOWDATA="
+#define DO_ACTION           "0-DOACTION="
+#define DO_ACTION_LENGTH    11
+#define SHOW_DATA           "1-SHOWDATA="
+#define SHOW_DATA_LENGTH    11
+#define END_OF_TASK         "2-ENDOFTASK="
+#define END_OF_TASK_LENGTH  12
 
 ////////////
 //
@@ -30,8 +34,12 @@
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 // Variable to read the status of push button
 int button_state = 0;   
+
+// Flag indicating if lauched task has been reported done
+bool task_is_done = true;
 
 void setup() {
 
@@ -72,23 +80,45 @@ void loop() {
 
   // Write on serial interface when button pressed 
   button_state = digitalRead(PUSH_BUTTON);
-  if (button_state == HIGH) {
-    delay(500);
+  if (button_state == HIGH && task_is_done) {
+    
+    // Telling the user his command is being sent
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.write("Sending...");
+    display.display();
+    delay(2000);
+    
+    // Changing flag state to avoid multiple calls of the same task
+    task_is_done = false;
+
+    // Sending order
     Serial.println(DO_ACTION);
   }
 
-  // When data received through serial interface, show what must be shown on screen
+  // Wait for something on serial interface
   if (Serial.available() > 0) {
     String serialRead = Serial.readString();
     serialRead.trim();
 
     if (serialRead.startsWith(SHOW_DATA)) {
-
-      // Show received data
-      String rawData = serialRead.substring(11, serialRead.length());
+      // Display received data on screen
+      String rawData = serialRead.substring(SHOW_DATA_LENGTH, serialRead.length());
       display.clearDisplay();
+      display.setCursor(0, 0);
       display.write(rawData.c_str());
       display.display();
+    }
+    else if (serialRead.startsWith(END_OF_TASK)) {
+      // Display that the previously launched task is now done
+      String rawData = serialRead.substring(END_OF_TASK_LENGTH, serialRead.length());
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.write("Done");
+      display.display();
+
+      // Changing flag state to allow the user to launch tasks again
+      task_is_done = true;
     }
   }
 }
--- Server/remote_backup_laucher.py
+++ Server/remote_backup_laucher.py
@@ -1,17 +1,44 @@
-# Importing Libraries 
 import serial 
 import time 
 import subprocess
 import logging
 
+################
+#
+# Constants
+#
+################
+
+DO_ACTION = "0-DOACTION="
+SHOW_DATA = "1-SHOWDATA="
+END_OF_TASK = "2-ENDOFTASK="
+
+################
+#
+# Init
+#
+################
+
+# Initialize serial interface
 serial_device = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1) 
+
+################
+#
+# Functions
+#
+################
 
 def sendMessage(message_content): 
     serial_device.write(bytes("1-SHOWDATA=" + message_content, 'utf-8')) 
     time.sleep(0.05) 
 
-#result = subprocess.run(['bash', './test/fake-script-output-small.sh'], stdout=subprocess.PIPE)
+################
+#
+# Loop
+#
+################
 
+# Launching the script
 p = subprocess.Popen(['bash', './test/fake-script-output-small.sh'], stdout=subprocess.PIPE, text=True)
 
 while True: 
@@ -26,9 +53,11 @@ while True:
 
         # Wait at least two second between to serial writes
         epoch_time = int(time.time())
-        if epoch_time > last_serialwrite_epoch_time + 1 :
+        if epoch_time > last_serialwrite_epoch_time :
             logging.warning(last_stdout_line)
             sendMessage(last_stdout_line) 
+            last_serialwrite_epoch_time = epoch_time
+
     else:
         # Has finished
         time.sleep(1);
